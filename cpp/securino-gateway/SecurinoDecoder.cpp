4f63a7b0bcb1c86be1c8751efbaa3a6831aa288f
DorianThiv <dorian_thivolle@orange.fr>
2018-05-31 16:32:10 +0000

    Updtae decoder for dht sensor

--- cpp/securino-gateway/SecurinoDecoder.cpp
+++ cpp/securino-gateway/SecurinoDecoder.cpp
@@ -3,28 +3,50 @@
 void SecurinoDecoder::decode(char * destbuf, uint8_t * frame, uint8_t size)
 {
     if (frame[1] == TYPE_SENSOR_DHT)
-        decodeDHT(destbuf, frame, size);
+    {
+        if (frame[2] == TYPE_SENSOR_DHT_TEMP)
+            decodeDhtTemp(destbuf, frame, size);
+        else if (frame[2] == TYPE_SENSOR_DHT_HUMIDITY)
+            decodeDhtHumi(destbuf, frame, size);
+    }
     else if (frame[1] == TYPE_SENSOR_LAZER)
         decodeLazer(destbuf, frame, size);
 }
 
-void SecurinoDecoder::decodeDHT(char * destbuf, uint8_t * frame, uint8_t size)
+void SecurinoDecoder::decodeDhtTemp(char * destbuf, uint8_t * frame, uint8_t size)
 {
-    printf("Decode DHT\n");
-    int i = 0;
-    while (i < size)
-    {
-        i++;
-    }
+    printf("=> Decode DHT Temperature ...\n");
+
+    int temperature = frame[B_SENSOR_DHT_TEMP];
+
+    const char * f_temp = "{ \"device\": \"Arduino Uno\", \"sensor\": \"temperature\", \"value\": %d, \"unit\": \"°C\" }";
+    sprintf(destbuf, f_temp, temperature);
+
+    printf("Temperature : %d °C\n", temperature);
 }
+
+void SecurinoDecoder::decodeDhtHumi(char * destbuf, uint8_t * frame, uint8_t size)
+{
+    printf("=> Decode DHT Humidity ...\n");
+
+    int humidity = frame[B_SENSOR_DHT_HUMIDITY];
+
+    const char * f_humi = "{ \"device\": \"Arduino Uno\", \"sensor\": \"humidity\", \"value\": %d, \"unit\": \"%\" }";
+    sprintf(destbuf, f_humi, humidity);
+
+    printf("Humidity : %d \n", humidity);
+}
+
 
 void SecurinoDecoder::decodeLazer(char * destbuf, uint8_t * frame, uint8_t size)
 {
-    printf("Decode Lazer\n");
-    int i = 0;
-    while (i < size)
-    {
-        i++;
-    }
+    printf("=> Decode Lazer\n");
+
+    int status = frame[B_SENSOR_LAZER_VALUE];
+
+    const char * f_res = "{ \"device\": \"Arduino Uno\", \"sensor\": \"Lazer\", \"value\": %d, \"unit\": \"\" }";
+    sprintf(destbuf, f_res, status);
+
+    printf("Laser status : %d\n", status);
 }
 
--- cpp/securino-gateway/SecurinoDecoder.h
+++ cpp/securino-gateway/SecurinoDecoder.h
@@ -1,3 +1,21 @@
+/**********************************************************
+*
+*                    Securino Decoder
+*
+*                    THIVOLLE Dorian
+*                dorian_thivolle@orange.fr
+*                         CASIR
+*
+***********************************************************/
+
+/**********************************************************
+*                    Securino Decoder
+* ---------------------------------------------------------
+* Decode frame from arduino
+* Specs:
+*   - 0x41 0x42 0x1a 0x17 : DHT frame
+*   - 0x41 0x4c 0x00      : Lazer frame
+**********************************************************/
 
 #ifndef __SECURINO_DECODER_H__
 #define __SECURINO_DECODER_H__
@@ -5,11 +23,19 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#define B_DEVICE 0;
-#define B_SENSOR 1;
+#include <iostream>
 
-#define TYPE_SENSOR_DHT 66;
+#define B_DEVICE 0
+#define B_SENSOR 1
+
+#define TYPE_SENSOR_DHT 66
+#define TYPE_SENSOR_DHT_TEMP 84
+#define TYPE_SENSOR_DHT_HUMIDITY 72
 #define TYPE_SENSOR_LAZER 76
+
+#define B_SENSOR_DHT_TEMP 2
+#define B_SENSOR_DHT_HUMIDITY 3
+#define B_SENSOR_LAZER_VALUE 2
 
 class SecurinoDecoder {
 
@@ -18,12 +44,14 @@ class SecurinoDecoder {
         SecurinoDecoder() { }
         ~SecurinoDecoder() { }
 
-        static void decode(uint8_t * destbuf, uint8_t * frame, uint8_t size);
+        static void decode(char * destbuf, uint8_t * frame, uint8_t size);
 
     private:
 
-        static void decodeDHT(uint8_t * destbuf, uint8_t * frame, uint8_t size);
-        static void decodeLazer(uint8_t * destbuf, uint8_t * frame, uint8_t size);
+        static void decodeDhtTemp(char * destbuf, uint8_t * frame, uint8_t size);
+        static void decodeDhtHumi(char * destbuf, uint8_t * frame, uint8_t size);
+        static void decodeLazer(char * destbuf, uint8_t * frame, uint8_t size);
+
 };
 
 #endif // __SECURINO_DECODER_H__
